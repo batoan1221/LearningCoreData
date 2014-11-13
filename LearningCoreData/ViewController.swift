ca6ba68ba15efdf0fa97a3f16317004a2329037d
Ba Toan <batoan1221@gmail.com>
2014-11-13 14:34:36 +0700

    Initial Commit

---
+++ LearningCoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearningCoreData
+//
+//  Created by iSlan on 11/13/14.
+//  Copyright (c) 2014 2359 Media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
